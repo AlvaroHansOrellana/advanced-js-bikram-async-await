e91c4d9c0e33dae190a76f78b627565d7320b01c
Hans <alvarohansorellana@gmail.com>
2024-10-09 13:42:59 +0200

    updated last tests

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const getRandomDogImage = async () => {
     }
 }
 
-getRandomDogImage().then(item => console.log(item));
+// getRandomDogImage().then(item => console.log(item));
 
 //EJERCICIO 05  
 
@@ -100,61 +100,103 @@ const getRandomPokemonImage = async () => {
 
 //EJERCICIO 06
 
-const printPugVsPikachu = async () => {
+// const printPugVsPikachu = async () => {
+//     try {
+//         let dogResponse = await fetch('https://dog.ceo/api/breeds/image/random');
+//         let pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+//         if (!dogResponse.ok) {
+//             throw new Error(`Error HTTP: ${dogResponse.status} - ${dogResponse.statusText}`);
+//         }
+//         if (!pokeResponse.ok) {
+//             throw new Error(`Error HTTP: ${pokeResponse.status} - ${pokeResponse.statusText}`);
+//         }
+//         let dogData = await dogResponse.json();
+//         let dogImg = dogData.message;
+//         let pokeData = await pokeResponse.json();
+//         let pokeImg = pokeData.sprites.front_default;
+//         let vs = "https://imgs.search.brave.com/G9N1fEk2XqKORl-MFJ-mJL2BkHl93WrHPnXecsG3TBw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nbWFydC5jb20v/ZmlsZXMvMTEvVmVy/c3VzLVBORy1IRC5w/bmc";
+//         const createImageContainer = (images) => {
+//             let container = document.createElement('div');
+//             container.setAttribute('id', 'pug-vs-pikachu');
+//             container.style.display = 'flex';
+        
+//             for (let i = 0; i < images.length; i++) {
+//                 const { src, alt, width } = images[i];
+//                 container.appendChild(createImageElement(src, alt, width));
+//             }
+        
+//             return container;
+//         };
+        
+//         let dogImageElement = document.createElement('img');
+//         dogImageElement.src = dogImg;
+         
+//         let vsImageElement = document.createElement('img');
+//         vsImageElement.src = vs;
+
+//         let pokeImageElement = document.createElement('img');
+//         pokeImageElement.src = pokeImg;
+
+//         container.appendChild(dogImageElement);
+//         container.appendChild(vsImageElement);
+//         container.appendChild(pokeImageElement);
+
+//         // Insertar el contenedor en el cuerpo del documento
+//         document.body.appendChild(container);
+//     } catch (error) {
+//         console.error('Hubo un error en la segunda solicitud', error.message);
+//     };
+//    };
+
+// printPugVsPikachu();
+
+//Ejercicio 7.- Declara una función getRandomCharacter que retorne un personaje aleatorio.
+
+async function getRandomCharacter() {
     try {
-        let dogResponse = await fetch('https://dog.ceo/api/breeds/image/random');
-        let pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
-        if (!dogResponse.ok) {
-            throw new Error(`Error HTTP: ${dogResponse.status} - ${dogResponse.statusText}`);
-        }
-        if (!pokeResponse.ok) {
-            throw new Error(`Error HTTP: ${pokeResponse.status} - ${pokeResponse.statusText}`);
-        }
-        let dogData = await dogResponse.json();
-        let dogImg = dogData.message;
-        let pokeData = await pokeResponse.json();
-        let pokeImg = pokeData.sprites.front_default;
-        let vs = "https://imgs.search.brave.com/G9N1fEk2XqKORl-MFJ-mJL2BkHl93WrHPnXecsG3TBw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nbWFydC5jb20v/ZmlsZXMvMTEvVmVy/c3VzLVBORy1IRC5w/bmc";
-        const createImageContainer = (images) => {
-            let container = document.createElement('div');
-            container.setAttribute('id', 'pug-vs-pikachu');
-            container.style.display = 'flex';
-        
-            for (let i = 0; i < images.length; i++) {
-                const { src, alt, width } = images[i];
-                container.appendChild(createImageElement(src, alt, width));
-            }
-        
-            return container;
-        };
-        // contenedor div para las imágenes
-
-        // Crear la imagen del perro
-        let dogImageElement = document.createElement('img');
-        dogImageElement.src = dogImg;
-        dogImageElement.style.width = '150px'; // Opcional: tamaño
-        
-
-        // Crear la imagen "VS"
-        let vsImageElement = document.createElement('img');
-        vsImageElement.src = vs;
-        vsImageElement.style.width = '100px'; // Opcional: tamaño
-
-        // Crear la imagen de Pikachu
-        let pokeImageElement = document.createElement('img');
-        pokeImageElement.src = pokeImg;
-        pokeImageElement.style.width = '200px'; // Opcional: tamaño
-
-        // Añadir las imágenes al contenedor
-        container.appendChild(dogImageElement);
-        container.appendChild(vsImageElement);
-        container.appendChild(pokeImageElement);
-
-        // Insertar el contenedor en el cuerpo del documento
-        document.body.appendChild(container);
+      let response = await fetch(`https://rickandmortyapi.com/api/character/${Math.floor(Math.random() * 827)}`)
+  
+      let data = await response.json();
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status} - ${response.statusText}`);
+      }
+      return data
     } catch (error) {
-        console.error('Hubo un error en la segunda solicitud', error.message);
-    };
-   };
+      // Manejar errores de red o del servidor
+      console.error('Hubo un problema con la solicitud:', error.message);
+    }
+  }
 
-printPugVsPikachu();
+
+//   Ejercicio 8.- Declara una función getRandomCharacterInfo que retorne de un personaje su imagen, nombre, episodios en los que aparece y el nombre del primer episodio en el que aparece + fecha de estreno, tendrás que hacer otro fetch para llegar a los ultimos datos. Formato de retorno => (return {img, name, episodes, firstEpisode, dateEpisode})
+
+const getRandomCharacterInfo = async () => {
+    try {
+        let response = await fetch('https://rickandmortyapi.com/api/character/4');
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error (`Error HTTP: ${response.status} - ${response.statusText}`);
+        }
+        let img = data.image;
+        console.log(img)
+        let name = data.name;
+        console.log(name)
+        let episodes = data.episode;
+        let episodeLength = episodes.length;
+        console.log(episodes)
+        let firstEpisode = data.episode[0]
+        console.log(firstEpisode)
+
+        let secondResponse = await fetch ('https://rickandmortyapi.com/api/episode/6');
+        const secondData = await secondResponse.json();
+        if (!secondResponse.ok) {
+            throw new Error (`Error HTTP: ${secondResponse.status} - ${secondResponse.statusText}`);
+        }
+        let firstEpisodeName = await fetch (secondResponse);
+        let airDate = secondData.air_date;
+        return {img, name, episodes, firstEpisodeName, airDate, episodeLength};
+    } catch (error) {
+        console.error('Hubo un problema con la solicitud:', error.message);
+    }
+}
+
